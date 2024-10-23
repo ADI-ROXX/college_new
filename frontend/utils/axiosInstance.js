921f64a10af3781f40903160cf032dff9e3f2aef
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2024-10-24 02:11:51 +0530

    fix nextjs env issue

--- frontend/utils/axiosInstance.js
+++ frontend/utils/axiosInstance.js
@@ -1,25 +1,25 @@
 // frontend/utils/axiosInstance.js
-import axios from 'axios';
-
+import axios from 'axios'
+export const NEXT_PUBLIC_BACKEND_URL = 'http://localhost:5080/api'
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: NEXT_PUBLIC_BACKEND_URL,
   headers: {
-    'Content-Type': 'application/json',
-  },
-});
+    'Content-Type': 'application/json'
+  }
+})
 
 // Add a request interceptor to include the JWT token if available
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token')
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`
     }
-    return config;
+    return config
   },
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default axiosInstance;
+export default axiosInstance
